a4d49b3c0fe850ff88844d941fb46204078a5ae5
TahLoow <35899454+TahLoow@users.noreply.github.com>
2019-01-30 22:52:48 -0500

    Create Server_Container.lua

---
+++ FactoryTemplates/Container/Server_Container.lua
@@ -0,0 +1,36 @@
+local GM = require(game.ReplicatedStorage.GlobalMethods)
+
+local ClassData = {
+	ReadWrite = {},
+	ReadOnly = {
+		ClassName = "Container",
+		Contents = "",
+		Capacity = 0,
+		Quantity = 0,
+		
+		SetCapacity = function(self,NewValue)
+			if type(NewValue) ~= "number" then return end
+			self.Capacity = NewValue
+		end,
+		SetQuantity = function(self,NewValue)
+			if type(NewValue) ~= "number" then return end
+			self.Quantity = math.clamp(NewValue,0,self.Capacity)
+		end,
+		UpdateQuantity = function(self,Delta)
+			self:SetQuantity(self.Quantity + Delta)
+		end,
+	},
+	Events = {},
+	
+	Initialize = function(self,Contents,Capacity,Quantity)
+		self.Contents = GM:TernaryTypeAssert(Contents,"string",Contents,"Unknown")
+		self.Capacity = GM:TernaryTypeAssert(Capacity,"number",Capacity,0)
+		self.Quantity = GM:TernaryTypeAssert(Quantity,"number",Quantity,0)
+		
+		return
+	end,
+	
+	Dependencies = {}
+}
+
+return ClassData
